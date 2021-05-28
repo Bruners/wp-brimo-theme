09736f4e90f38bd0188462f5b6b4dc805d7b24f1
Lasse Brun <bruners@gmail.com>
2021-05-29 00:30:12 +0200

    Fix version constant from _s on woocommerce.php

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -43,7 +43,7 @@ add_action( 'after_setup_theme', 'brimo_woocommerce_setup' );
  * @return void
  */
 function brimo_woocommerce_scripts() {
-	wp_enqueue_style( 'brimo-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), _S_VERSION );
+	wp_enqueue_style( 'brimo-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), BRIMO_VERSION );
 
 	$font_path   = WC()->plugin_url() . '/assets/fonts/';
 	$inline_font = '@font-face {
